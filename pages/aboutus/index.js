451f6b980a22c73cca910552d393f31483d7a7da
Arzun000 <me.arzun@gmail.com>
2022-03-14 17:46:00 +0545

    modified:   pages/aboutus/Ourteam.js 	modified:   pages/aboutus/Ourteam.module.css 	modified:   pages/aboutus/index.js 	pages/aboutus/Content.js 	public/aboutus/icons/Facebook.png 	public/aboutus/icons/facebook1.png 	public/aboutus/icons/linkedin.png 	public/aboutus/icons/linkedin1.png 	public/aboutus/icons/twitter.png 	public/aboutus/icons/twitter1.png 	public/aboutus/staffs/

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -4,6 +4,8 @@ import { Headertwo } from "../../components/Headertwo";
 import { Headerthree } from "../../components/Headerthree";
 import { Body } from "./Body.js";
 import { Ourteam } from "./Ourteam.js";
+import { Content } from "./Content.js";
+import { Footer } from "../../components/Footer";
 export default function aboutus() {
   return (
     <div>
@@ -12,6 +14,8 @@ export default function aboutus() {
       <Headerthree />
       <Body />
       <Ourteam />
+      <Content />
+      <Footer />
     </div>
   );
 }
